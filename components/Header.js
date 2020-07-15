815278695402035f8c51d609603a874c6b34cde2
Steven <stevenklavins94@gmail.com>
2020-07-15 22:46:08 +0100

    chore: imported colours

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Colours from '../constants/colours'
 
 const Header = (props) => {
   return (
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: '#2E4053',
+    backgroundColor: Colours.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
