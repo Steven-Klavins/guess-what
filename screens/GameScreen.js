4b5f800da3c24e66126f48a2702875740463c7d2
Steven <stevenklavins94@gmail.com>
2020-07-16 21:24:16 +0100

    chore: added game screen component

---
+++ screens/GameScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Colours from '../constants/colours'
+
+const GameScreen = (props) => {
+  return <View></View>
+}
+
+const styles = StyleSheet.create({})
+
+export default GameScreen
