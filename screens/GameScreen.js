98ed4ca3842c9201cd462552f5f278d65632c1e8
Steven <stevenklavins94@gmail.com>
2020-07-16 21:34:51 +0100

    feat: added random number functionality

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Colours from '../constants/colours'
 
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude)
+  } else {
+    return rndNum
+  }
+}
+
 const GameScreen = (props) => {
-  return <View></View>
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice),
+  )
 }
 
 const styles = StyleSheet.create({})
