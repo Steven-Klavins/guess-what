8d5b9dbf4b0485d4e9996bd9d8d6b026f2985264
Steven <stevenklavins94@gmail.com>
2020-07-17 23:57:10 +0100

    Styling: updated font

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import TitleText from '../constants/TitleText'
+import BodyText from '../constants/BodyText'
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Game Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>Game Over!</TitleText>
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="New Game" onPress={props.onRestart}></Button>
     </View>
   )
