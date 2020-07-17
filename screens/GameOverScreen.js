61b9d5917bce9833cc90cc5dac3051cd151e3fd5
Steven <stevenklavins94@gmail.com>
2020-07-17 20:42:48 +0100

    feat: added reset button and score output

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,6 +5,9 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>Game Over!</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="New Game" onPress={props.onRestart}></Button>
     </View>
   )
 }
