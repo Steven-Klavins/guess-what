b60e1ee60d75b632dd4e4ae320ba8bc1dfa9b1ec
Steven <stevenklavins94@gmail.com>
2020-07-18 00:40:06 +0100

    chore: imported image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
 import TitleText from '../constants/TitleText'
 import BodyText from '../constants/BodyText'
 
@@ -7,6 +7,11 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>Game Over!</TitleText>
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={require('../assets/images/robodog.png')}
+      />
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="New Game" onPress={props.onRestart}></Button>
@@ -19,6 +24,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  image: {
+    width: '80%',
+    height: 300,
   },
 })
 
