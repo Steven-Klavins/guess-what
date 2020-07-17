b5da713691d3e04e17fac695a067b3fd95de46ce
Steven <stevenklavins94@gmail.com>
2020-07-17 16:24:12 +0100

    feat: added component return for game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import Colours from '../constants/colours'
+import NumberContainer from '../components/NumberContainer'
+import Card from '../components/Card'
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min)
@@ -18,8 +20,32 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice),
   )
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="Lower" onPress={() => {}} />
+        <Button title="Greater" onPress={() => {}} />
+      </Card>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%',
+  },
+})
 
 export default GameScreen
