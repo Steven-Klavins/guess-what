d13def98729173233f65a837b085e1e31325ef52
Steven <stevenklavins94@gmail.com>
2020-07-17 20:42:18 +0100

    chore: passed props to handle new game

--- App.js
+++ App.js
@@ -9,9 +9,13 @@ export default function App() {
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
 
+  const newGameHandler = () => {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber)
-    setGuessRounds(0)
   }
 
   const gameOverHandler = (numberOfRounds) => {
@@ -25,7 +29,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     )
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={newGameHandler}
+      />
+    )
   }
   return (
     <View style={styles.screen}>
